0b8717c06fcb84ae8a14e5df21ac8abe6b09d2b0
Unknown <Calvin.lapp@dcmail.ca>
2020-03-02 13:59:31 -0500

    Start of Backend file
    Started creating skeleton for Backend.java file.  Adding blank mehtods and attributes

---
+++ Backend.java
@@ -0,0 +1,30 @@
+/**
+ * Backend class.
+ * Deals with input from a file and parses data to send out
+ * to another file.
+ * 
+ * @author Luke Tran, Sean Caldwell, Calvin Lapp
+ * @since March 2nd, 2020
+ * @version 1.0
+ * @name Backend.java
+ */
+
+import java.util.Vector;
+
+/**
+ * Main function
+ * @param
+ * @return
+ */
+class Backend {
+
+    /* Attributes */
+    private Vector<String> users;
+    private Vector<String> transactions;
+    private Vector<String> items;
+
+    /* Methods */
+    public static void main(String[] args) {
+        System.out.println("yeehaw.");
+    }
+}
