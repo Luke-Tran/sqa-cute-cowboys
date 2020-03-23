a1ba5f3df25c7326993fb700b2441221e3cd15de
Luke-Tran <luketran08@gmail.com>
2020-03-23 17:09:23 -0400

    Added a JUnit test case

---
+++ tests/UserTests.java
@@ -0,0 +1,27 @@
+package tests;
+
+import backend.Backend;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+import java.util.Vector;
+
+/*
+ * Please compile from the root directory.
+ * To compile the backend:
+   $ javac backend/*.java
+ * To compile this file:
+   $ javac -d tests/output/ -cp .:<path to junit-4.12.jar> tests/UserTests.java
+ * To run the test:
+   $ java -cp .:/:tests/output/:<path to hamcrest-core-1.3.jar>:<path to junit-4.12.jar> org.junit.runner.JUnitCore tests.UserTests
+ */
+
+public class UserTests {
+	@Test
+	public void usersVectorNotEmptyAfterAdd() {
+		Backend backend = new Backend();
+		backend.setUsers(new Vector<String>());
+		backend.addNewUser("01 a               FS 000000.00");
+		assertTrue(backend.getUsers().size() > 0);
+	}
+}
