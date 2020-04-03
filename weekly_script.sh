eb1cce0c5b404b3ca4392eda5342aa491fde230a
Luke-Tran <luketran08@gmail.com>
2020-04-03 11:10:36 -0400

    Finished writing weekly script

--- weekly_script.sh
+++ weekly_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# The weekly script that runs the daily script five times on different sets of transactions,
+# simulating 5 days of using the system.
+
 for i in {1..5}; do
-	echo "day$i"
+	./daily_script.sh "day$i"
 done
